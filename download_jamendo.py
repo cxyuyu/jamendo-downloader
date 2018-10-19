e021c3068716405a3152187e976675b8a002350c
cxyu <996702172@qq.com>
2018-10-19 18:23:18 +0800

    MOD:取消下载时的时间休息，添置中断后重新下载逻辑

--- download_jamendo.py
+++ download_jamendo.py
@@ -4,7 +4,7 @@ import re
 import os
 
 
-def get_():
+def get_(channel_num= {'emotional': 0, 'dance': 0, 'chillout': 0, 'ambient': 0, 'energetic': 0}):
     '''
 
     :return:
@@ -12,7 +12,7 @@ def get_():
     #cookie
     header = {
         "user-agent": " Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36",
-        'x-jam-call': '$a6b3c3c8a5a1888b1a41c29517f45ec214621aff*0.5574606687572872~'}
+        'x-jam-call': '$92525533bfd98eba6344f72433e52a2991e122d0*0.7073001144863946~'}
     url = 'https://www.jamendo.com/community/punk/tracks'
     response = requests.get(url, headers=header)
     response.cookies.set("_ga", "GA1.2.1870680278.1539831081", path="www.jamendo.com")
@@ -32,18 +32,24 @@ def get_():
     Energetic
     https://www.jamendo.com/api/charts/track?type%5B%5D=track&tagId=5942&limit=20
     '''
-    channel_id = {'emotional': 2754, 'dance': 100, 'chillout': 1834, 'ambient': 362, 'energetic': 5942}
-    for channelname in channel_id:
+    channel_key = {'emotional': 2754, 'dance': 100, 'chillout': 1834, 'ambient': 362, 'energetic': 5942}
+    channel_ids = {}
+    for channelname in channel_key:
         print(channelname)
-        channel_id = channel_id[channelname]
+        channel_id = channel_key[channelname]
         response = requests. \
             get("https://www.jamendo.com/api/charts/track?type%5B%5D=track&tagId={}&limit=30".format(channel_id),
                 cookies=response.cookies, headers=header)
         print(response.text)
+        time.sleep(5)
         ids = re.findall('"id":(\d+),', response.text)
-        print(ids)
-        #
-        save_music(channelname, ids)
+        print(len(ids[channel_num[channelname]:]))
+        print(ids[channel_num[channelname]:])
+        ids=ids[channel_num[channelname]:]
+        channel_ids[channelname]=ids
+    print(channel_ids)
+    # for key in channel_ids:
+    #     save_music(key, channel_ids[key])
 
 
 def save_music(path, ids):
@@ -98,9 +104,13 @@ curl 'https://www.jamendo.com/api/charts/track?type%5B%5D=track&tagId=2754&limit
  -H 'x-jam-call: $a6b3c3c8a5a1888b1a41c29517f45ec214621aff*0.5574606687572872~' --compressed
 
 
-curl 'https://www.jamendo.com/api/charts/track?type%5B%5D=track&tagId=2754&limit=55' -H 'pragma: no-cache' -H 'cookie: jammusiclang=en; jammusicsession=s%3A8NhmQGgR3StOP3GkQ-jEtx8zMnEPzfPS.k%2FfuPj8m5cvrTl3VsMtKczp%2BTPiph3iIhBF79yS02FA; _ga=GA1.2.1870680278.1539831081; _gid=GA1.2.1391188083.1539831081; _hjIncludedInSample=1; jamapplication=true; jamAcceptCookie=true'  -H 'x-jam-call: $a6b3c3c8a5a1888b1a41c29517f45ec214621aff*0.5574606687572872~' --compressed
+curl 'https://www.jamendo.com/api/charts/track?type%5B%5D=track&tagId=362&limit=55' -H 'pragma: no-cache' -H 'cookie: jammusiclang=en; jammusicsession=s%3A8NhmQGgR3StOP3GkQ-jEtx8zMnEPzfPS.k%2FfuPj8m5cvrTl3VsMtKczp%2BTPiph3iIhBF79yS02FA; _ga=GA1.2.1870680278.1539831081; _gid=GA1.2.1391188083.1539831081; _hjIncludedInSample=1; jamapplication=true; jamAcceptCookie=true'  -H   'x-jam-call: $92525533bfd98eba6344f72433e52a2991e122d0*0.7073001144863946~' --compressed
+curl 'https://www.jamendo.com/api/charts/track?type%5B%5D=track&tagId=362&limit=55' -H 'pragma: no-cache' -H 'cookie: jammusicsession=s%3Au5Fm1aez98OH7-8RoLAH0EBMG02wrVDR.8IeGTtnAxemQGmGnsyLoyLXkDcoWCUYudQgxLC4XxQA; jammusiclang=en; _ga=GA1.2.680150216.1539836117; _gid=GA1.2.363062877.1539836117; _hjIncludedInSample=1; jamapplication=true; jamAcceptCookie=true; global-app_hash=b7cb2682057357e5d8b0c8e0b9882e21d4718331aa58f811b6b8e127947eb482ff29924468d68db92b9ba625eb066b05d9d90ff10db69e82a4cf5817a557f306773c1d00b970f77887' -H 'x-jam-version: xrgpd' -H 'accept-encoding: gzip, deflate, br' -H 'accept-language: zh-CN,zh;q=0.9' -H 'user-agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36' -H 'accept: application/json, text/javascript, */*; q=0.01' -H 'cache-control: no-cache' -H 'authority: www.jamendo.com' -H 'x-requested-with: XMLHttpRequest' -H 'referer: https://www.jamendo.com/community/ambient' -H 'x-jam-call: $92525533bfd98eba6344f72433e52a2991e122d0*0.7073001144863946~' --compressed
 '''
 
 if __name__ == "__main__":
     channel_num = {'emotional': 0, 'dance': 0, 'chillout': 0, 'ambient': 0, 'energetic': 0}
-    get_()
+    # 输入数值如
+    # {'emotional': 10, 'dance': 20, 'chillout': 0, 'ambient': 0, 'energetic': 0}
+    # 默认下载30首，设置数值的原因（因网速不好，可能导致的中断较多），中断后需要继续可以防止重新下载
+    get_(channel_num)
