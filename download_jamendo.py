04b17fd6ded3fe480979bfad7f9b68792783dff8
cxyu <996702172@qq.com>
2018-10-20 11:04:03 +0800

    MOD:将数据序列化，应对同样的headers对应不同的数据数量，实现开始抓取就能获取全部音乐

--- download_jamendo.py
+++ download_jamendo.py
@@ -2,8 +2,10 @@ import requests
 import time
 import re
 import os
+import argparse
+import pickle
 
-
+#第一步先是将数据序列化，之后再获取更为方便一些
 def get_(channel_num= {'emotional': 0, 'dance': 0, 'chillout': 0, 'ambient': 0, 'energetic': 0}):
     '''
 
@@ -11,7 +13,7 @@ def get_(channel_num= {'emotional': 0, 'dance': 0, 'chillout': 0, 'ambient': 0, 
     '''
     #cookie
     header = {
-        "user-agent": " Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36",
+        "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36",
         'x-jam-call': '$92525533bfd98eba6344f72433e52a2991e122d0*0.7073001144863946~'}
     url = 'https://www.jamendo.com/community/punk/tracks'
     response = requests.get(url, headers=header)
@@ -48,9 +50,17 @@ def get_(channel_num= {'emotional': 0, 'dance': 0, 'chillout': 0, 'ambient': 0, 
         ids=ids[channel_num[channelname]:]
         channel_ids[channelname]=ids
     print(channel_ids)
-    for key in channel_ids:
-        save_music(key, channel_ids[key])
+    # for key in channel_ids:
+    #     save_music(key, channel_ids[key])
+    with open("channel_ids","wb") as f:
+        pickle.dump(channel_ids,f)
 
+def load_dump():
+    if os.path.exists("channel_ids"):
+        with open("channel_ids","rb") as f:
+            channel_ids=pickle.dump(f)
+            for key in channel_ids:
+                save_music(key,channel_ids[key])
 
 def save_music(path, ids):
     if not os.path.exists(path):
@@ -59,7 +69,7 @@ def save_music(path, ids):
 
 
 def get_music(path, ids):
-    count = 0
+    count = 30-len(ids)
     for id in ids:
         song_name = get_name(id)
         # time.sleep(1)
@@ -67,10 +77,13 @@ def get_music(path, ids):
             "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.186 Safari/537.36"}
         url = 'https://mp3d.jamendo.com/?trackid={}&format=mp32&from=app-97dab294'.format(id)
         now_date = time.strftime("%Y:%m:%d", time.localtime())
+        time_start=time.time()
         print("start download {}".format(song_name))
         with open(path + '/{}-{}-{}.mp3'.format(now_date, count, song_name), 'wb') as f:
             response = requests.get(url, headers=header)
             f.write(response.content)
+        time_end=time.time()
+        print("下载 {}耗时".format(song_name,str(time_end-time_start)))
         count = count + 1
         # time.sleep(3)
 
@@ -109,8 +122,12 @@ curl 'https://www.jamendo.com/api/charts/track?type%5B%5D=track&tagId=362&limit=
 '''
 
 if __name__ == "__main__":
-    channel_num = {'emotional': 0, 'dance': 0, 'chillout': 0, 'ambient': 0, 'energetic': 0}
+    channel_num = {'emotional': 30, 'dance': 27, 'chillout': 30, 'ambient': 30, 'energetic': 0}
     # 输入数值如
     # {'emotional': 10, 'dance': 20, 'chillout': 0, 'ambient': 0, 'energetic': 0}
     # 默认下载30首，设置数值的原因（因网速不好，可能导致的中断较多），中断后需要继续可以防止重新下载
+    parser = argparse.ArgumentParser()
+    #获取数据并序列化
     get_(channel_num)
+    #读取序列化数据
+    load_dump()
